ca71209844faa6a69831abc2195f51a15e821cf1
AJIT <AJIT@Ajit>
2022-11-07 12:10:37 +0530

    ok

---
+++ nov07th/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author AJIT
+ *
+ */
+module nov07th {
+}
---
+++ nov07th/src/nov07th/Test.java
@@ -0,0 +1,10 @@
+package nov07th;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+hgvg
+	}
+
+}
